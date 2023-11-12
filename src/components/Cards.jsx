056bed18859c0efc6373b84018598a5d41e961aa
fadelyking <fadely195@gmail.com>
2023-11-12 07:23:33 +0300

    reset score when a player clicks on the same card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Cards(props) {
+	const [score, setScore] = useState(0);
 	const [cards, setCards] = useState([
 		{ card: "Test 1", id: uuidv4(), clicked: false },
 		{ card: "Test 2", id: uuidv4(), clicked: false },
@@ -11,23 +12,50 @@ export default function Cards(props) {
 	]);
 
 	function handleClick(e) {
+		e.target.style.backgroundColor = "Red";
+
 		setCards(
 			cards.map((card) => {
-				if (e.target.getAttribute("value") === card.id) {
+				if (e.target.getAttribute("value") === card.id && card.clicked === false) {
 					return { ...card, clicked: true };
 				} else {
 					return card;
 				}
 			})
 		);
-		let clickedCards = 1;
-		for (let i = 0; i < cards.length - 1; i++) {
-			if (cards[i].clicked === true) clickedCards += 1;
+
+		let bestScoreArr = [];
+		for (let i = 0; i < 6; i++) {
+			if (cards[i].clicked === true) {
+				bestScoreArr.push(i);
+			}
 		}
-		console.log(clickedCards);
-		props.onClick({ clickedCards });
+
+		for (const card of cards) {
+			if (e.target.getAttribute("value") === card.id && card.clicked === false) {
+				setScore(score + 1);
+			} else if (e.target.getAttribute("value") === card.id && card.clicked === true) {
+				setCards(
+					cards.map((card) => {
+						if (e.target.getAttribute("value") === card.id && card.clicked === true) {
+							return { ...card, clicked: false };
+						} else {
+							return card;
+						}
+					})
+				);
+				return setScore(0);
+			}
+		}
+
+		props.onClick({ score, findBestScore });
 	}
 
+	/* 
+	useEffect(() => {
+		setCards(cards.map((card) => {}));
+	}, []);
+ */
 	return (
 		<div className="cards-container">
 			{cards.map((card) => (
