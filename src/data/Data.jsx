45da984a83826dc8b5f8b5a65fd65c9a7d7d3a23
fadelyking <fadely195@gmail.com>
2023-11-13 11:59:43 +0300

    fetching data from GIPHY API

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,1 +1,31 @@
-async function ReturnImages() {}
+import { useEffect, useState } from "react";
+
+export default function ReturnImages() {
+	const [gif, setGif] = useState([]);
+	const [defined, setDefined] = useState(false);
+
+	async function fetchGiphyData() {
+		try {
+			const response = await fetch(
+				`https://api.giphy.com/v1/gifs/translate?api_key=CgD3MYdRZc931mpYG58j9haL4EfRa3PG&s=cats`,
+				{ mode: "cors" }
+			);
+			const result = await response.json();
+			setGif(result);
+			setDefined(true);
+		} catch {
+			throw console.log("error");
+		}
+	}
+
+	useEffect(() => {
+		fetchGiphyData();
+	}, []);
+	if (defined === true) {
+		return (
+			<>
+				<img src={gif.data.images.original.url}></img>
+			</>
+		);
+	}
+}
